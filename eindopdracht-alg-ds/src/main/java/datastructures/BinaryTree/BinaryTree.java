65a5cdd26b9f8d49ac35815ae0dd76325404f519
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2023-03-13 21:41:22 +0100

    made class for binary tree and node for tree

--- eindopdracht-alg-ds/src/main/java/datastructures/BinaryTree/BinaryTree.java
+++ eindopdracht-alg-ds/src/main/java/datastructures/BinaryTree/BinaryTree.java
@@ -1,10 +1,13 @@
 package datastructures.BinaryTree;
 
+import assets.dataset.Person;
+
 public class BinaryTree {
     private Node root;
 
     public BinaryTree() {
         this.root = null;
     }
+
 
 }
